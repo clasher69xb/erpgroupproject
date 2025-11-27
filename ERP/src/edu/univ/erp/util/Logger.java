a839e4707d0f8db868d054f477c516206786720d
king <anotherfakeaddress4@gmail.com>
2025-11-27 17:00:44 +0530

    Updated logger

--- ERP/src/edu/univ/erp/util/Logger.java
+++ ERP/src/edu/univ/erp/util/Logger.java
@@ -1,5 +1,45 @@
-package util;
+
+package edu.univ.erp.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
     
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static boolean enabled = true;
+    
+    public static void setEnabled(boolean enabled) {
+        Logger.enabled = enabled;
+    }
+    
+    public static void info(String message) {
+        if (enabled) {
+            log("INFO", message);
+        }
+    }
+    
+    public static void warn(String message) {
+        if (enabled) {
+            log("WARN", message);
+        }
+    }
+    
+    public static void error(String message) {
+        if (enabled) {
+            log("ERROR", message);
+        }
+    }
+    
+    public static void error(String message, Exception e) {
+        if (enabled) {
+            log("ERROR", message + " - " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    private static void log(String level, String message) {
+        String timestamp = LocalDateTime.now().format(formatter);
+        System.out.println("[" + timestamp + "] [" + level + "] " + message);
+    }
 }
